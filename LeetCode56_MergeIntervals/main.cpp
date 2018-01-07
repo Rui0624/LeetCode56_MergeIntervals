07a9da3440d0bb861a92cc32bbb187e10ad54543
Rui Zhang <rui@dhcp154.merusouth116.iit.edu>
2018-01-07 17:47:19 -0600

    Initial Commit

---
+++ LeetCode56_MergeIntervals/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode56_MergeIntervals
+//
+//  Created by Rui on 1/7/18.
+//  Copyright © 2018 Rui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
