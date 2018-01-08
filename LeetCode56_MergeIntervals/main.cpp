e8dbe2b46eb90ddf7b4163360cb1f2c01c8f6fbf
Rui <rui@dhcp154.merusouth116.iit.edu>
2018-01-07 23:19:35 -0600

    first commit

--- LeetCode56_MergeIntervals/main.cpp
+++ LeetCode56_MergeIntervals/main.cpp
@@ -13,3 +13,40 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
 }
+
+
+
+ //Definition for an interval.
+    struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+ };
+ 
+class Solution {
+public:
+    static bool compare(const Interval &a, const Interval &b)
+    {
+        return (a.start < b.start); //create the compare used in the sort(), to make sure the sort of intervals will be in the increasing sort of start in each Intervals
+    }
+    
+    vector<Interval> merge(vector<Interval>& intervals) {
+        vector<Interval> res; //the result vector for return
+        if(intervals.empty())
+            return res;
+        sort(intervals.begin(), intervals.end(), compare); //sort the intervals gave by increasing order of start
+        res.push_back(intervals[0]); // push the first element into res
+        
+        for(int i = 1; i < intervals.size(); i++) //pass throught all the other intervals
+        {
+            if(res.back().end >= intervals[i].start) //compare the end of last element in res and the start of next element in intervals to find the overlaping;
+            {
+                res.back().end = max(res.back().end, intervals[i].end); //update the end of last element in the res, merger the possible overlapping
+            }else{
+                res.push_back(intervals[i]); //else put the new intervals into res
+            }
+        }
+        return res;
+    }
+};
